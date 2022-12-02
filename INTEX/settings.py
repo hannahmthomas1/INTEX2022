b4cadeff4e9f36be0398f3a8dc3b5df61c59230d
hannahmthomas1 <hannahmthomas1@gmail.com>
2022-12-01 23:07:45 -0700

    add database

--- INTEX/settings.py
+++ INTEX/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,16 +80,22 @@ WSGI_APPLICATION = 'INTEX.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'kidney_health',
-        'USER': 'postgres',
-        'PASSWORD': 'nacho',
-        'HOST': 'localhost',
-        'PORT': '5050'
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'kidney_health',
+#         'USER': 'postgres',
+#         'PASSWORD': 'nacho',
+#         'HOST': 'localhost',
+#         'PORT': '5050'
+#     }
+# }
+
 
 
 
