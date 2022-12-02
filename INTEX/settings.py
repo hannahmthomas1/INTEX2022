44e5b230be8d3073c23d41c256f087adb01b0e42
hannahmthomas1 <hannahmthomas1@gmail.com>
2022-12-02 10:31:21 -0700

    added http to trusted origins

--- INTEX/settings.py
+++ INTEX/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['kidney-health-INTEX.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://kidney-health-INTEX.up.railway.app']
 # CSRF_TRUSTED_ORIGINS = ['https://web-production-31c5.up.railway.app']
 
 # Application definition
